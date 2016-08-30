ab4a3f6fb253cffc1c9a0d531077299ed9845f1c
tdyakov <teodordyakov86@gmail.com>
2016-08-30 21:00:48 +0300

    Blog Project
    Blog Creation

---
+++ Demo-Blog/Classes/Utilis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo_Blog.Classes
+{
+    public class Utilis
+    {
+        public static string CutText(string text, int maxLength = 120)
+        {
+            if (text == null || text.Length <= maxLength)
+            {
+                return text;
+            }
+
+            var shortText = text.Substring(0, maxLength - 3) + "...";
+
+            return shortText;
+        }
+    }
+}
---
+++ Demo-Blog/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Demo_Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace Demo_Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            var latestPosts = db.Posts
+                .Include(p => p.Author)
+                .OrderByDescending(p => p.Date).Take(3);
+
+            return View(latestPosts);
+        }
+    }
+}
